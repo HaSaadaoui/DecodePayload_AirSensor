ba70677491f7be7eeccef74a8982a259ab950331
HaSaadaoui <hamza_saadaoui@outlook.fr>
2024-10-16 15:57:58 +0200

    feat : timestamp correction during frame decoding

--- Data_Clean_AirSensor.py
+++ Data_Clean_AirSensor.py
@@ -3,25 +3,41 @@ import subprocess
 import json
 from datetime import datetime
 import uuid  
-from dotenv import load_dotenv
-import os
 
-load_dotenv()  # take environment variables from .env.
+# Connexion à Cosmos DB
+CONNECTION_STRING = 'AccountEndpoint=https://cosmosdb-paris-chateaudun.documents.azure.com:443/;AccountKey=WQjxv3e4hNIOv2I91oLlTObR0PFVSSk3iB7goLJAMLwMEVCzgl98fmueRQEkwxBvqgmyBUmpXnfFACDbeUnUpg==;'
+client = CosmosClient.from_connection_string(CONNECTION_STRING)
+database = client.get_database_client('SmartBuildingDB-Paris-Chateaudun')
 
+# Conteneur source (données brutes)
+sensor_container = database.get_container_client('SensorData')
 
-# Fonction pour décoder une trame brute
-def decode_frame(raw_data):
+# Requête SQL pour récupérer les données brutes du capteur 'Air_05-01' pour une journée spécifique
+query = 'SELECT c.raw, c.ReceivedTimeStamp, c.metadata FROM c WHERE c.device = "Air_05-01" AND STARTSWITH(c.ReceivedTimeStamp, "2024-10-15")'
+
+# Exécution de la requête pour obtenir les éléments
+items = list(sensor_container.query_items(query=query, enable_cross_partition_query=True))
+
+# Affichage du nombre de trames trouvées
+print(f"Nombre de payloads trouvés : {len(items)}")
+
+# Conteneur de destination pour les données nettoyées et décodées
+destination_container = database.get_container_client('DataCleanCosmos')
+
+def decode_frame(raw_data, timestamp):
+    # Définir les arguments de la commande
     command = [
         "python",
-        "C:/Users/Codec-Report-Batch-Python-main/br_uncompress.py",  # Chemin vers votre script de décodage
+        "C:/Users/Codec-Report-Batch-Python-main/br_uncompress.py",
         "-a",
-        "3",           # paramètre pour ajuster selon votre script de décodage
-        "1,10,7,T",    # T sera renommé plus tard en Température
-        "2,100,6,H",   # H sera renommé plus tard en Humidité
-        "3,10,6,CO2",  # Décodage du CO2
-        "4,10,6,COV",  # Décodage des COV
+        "-t", timestamp,  # Utilisation de la variable timestamp ici
+        "3",              # Première partie de -a
+        "1,10,7,temperature",       # Deuxième partie de -a
+        "2,100,6,humidity",      # Troisième partie de -a
+        "3,10,6,CO2",     # Quatrième partie de -a
+        "4,10,6,COV",     # Cinquième partie de -a
         "-if",
-        raw_data       # la trame brute récupérée
+        raw_data          # Trame fournie par l'utilisateur
     ]
 
     try:
@@ -34,68 +50,53 @@ def decode_frame(raw_data):
         print("Sortie d'erreur :", e.stderr)
         return None
 
-# Connexion à Cosmos DB
-CONNECTION_STRING = os.getenv('CLE_COSMOS')
-client = CosmosClient.from_connection_string(CONNECTION_STRING)
-database = client.get_database_client('SmartBuildingDB-Paris-Chateaudun')
-
-# Conteneur source (données brutes)
-sensor_container = database.get_container_client('SensorData')
-
-# Requête SQL pour récupérer les données brutes du capteur 'Air_05-01' pour une journée spécifique
-query = 'SELECT c.raw, c.ReceivedTimeStamp FROM c WHERE c.device = "Air_05-01" AND STARTSWITH(c.ReceivedTimeStamp, "2024-10-10")'
-
-# Exécution de la requête pour obtenir les éléments
-items = list(sensor_container.query_items(query=query, enable_cross_partition_query=True))
-
-# Affichage du nombre de trames trouvées
-print(f"Nombre de payloads trouvés : {len(items)}")
-
-# Conteneur de destination pour les données nettoyées et décodées
-destination_container = database.get_container_client('DataCleanCosmos')
-
 # Parcourir et traiter chaque trame brute
 for item in items:
     raw_data = item['raw']
+    received_timestamp = item['ReceivedTimeStamp']  # Récupérer ReceivedTimeStamp
+    trimmed_timestamp = received_timestamp[:23] + "Z"  # Garder jusqu'à 3 chiffres de millisecondes et ajouter "Z"
     print(f"Trame brute : {raw_data}")
+    print(f"TimeStamp : {received_timestamp}")
+    print(f"trimmed_timestamp : {trimmed_timestamp }")
 
     # Décoder la trame brute
-    decoded_result = decode_frame(raw_data)
+    decoded_result = decode_frame(raw_data,trimmed_timestamp)
 
     if decoded_result:
         print("Résultat décodé avant modification des labels :")
         print(json.dumps(decoded_result, indent=4))
+   
+        if 'batch_absolute_timestamp' in decoded_result:
+                del decoded_result['batch_absolute_timestamp']
 
         # Préparer une nouvelle liste pour les données nettoyées
         cleaned_dataset = []
-
         # Parcourir le dataset 
         for data_point in decoded_result.get('dataset', []):
             # Suppression du champ data_relative_timestamp
             if 'data_relative_timestamp' in data_point:
-                del data_point['data_relative_timestamp']  
+                del data_point['data_relative_timestamp']            
             # Renommer les labels et ajuster les valeurs
-            if data_point['data']['label_name'] == 'T':
-                data_point['data']['label_name'] = 'Température'
+            if data_point['data']['label_name'] == 'temperature':
                 data_point['data']['value'] = round(data_point['data']['value'] * 0.01, 1)
-
-            elif data_point['data']['label_name'] == 'H':
-                data_point['data']['label_name'] = 'Humidité'
+            elif data_point['data']['label_name'] == 'humidity':
                 data_point['data']['value'] = data_point['data']['value'] * 0.01
 
             # Renommer data_absolute_timestamp
-            data_point['TimeStamp'] = data_point.pop('data_absolute_timestamp', '')  
-            
+            data_point['ReceivedTimeStamp'] = data_point.pop('data_absolute_timestamp', '')
             # Ajouter le data_point nettoyé à la nouvelle liste
             cleaned_dataset.append(data_point)
 
         # Mettre à jour le résultat décodé avec le dataset nettoyé
         decoded_result['dataset'] = cleaned_dataset
 
-        # Ajouter les colonnes 'id' et 'Type'
-        #decoded_result['id'] = item.get('ReceivedTimeStamp', datetime.now().isoformat())  # ou batch_relative_timestamp si disponible
-        decoded_result['id'] = str(uuid.uuid4())  # Génère un UUID comme identifiant
-        decoded_result['Type'] = "Air_05-01"
+        # Ajouter les colonnes 'id', 'Type', et 'ReceivedTimeStamp'
+        decoded_result['device'] = "Air_05-01"
+        decoded_result['deveui'] = "70B3D5E75E01C1FB"
+        decoded_result['BatchReceivedTimeStamp'] = item.get('ReceivedTimeStamp', {})
+        decoded_result['metadata'] = item.get('metadata', {})
+        decoded_result['id'] = str(uuid.uuid4())  
+
 
         # Afficher le document avant l'insertion
         print(f"Document à insérer : {json.dumps(decoded_result, indent=4)}")
@@ -105,3 +106,4 @@ for item in items:
         print(f"Données insérées dans le conteneur 'DataCleanCosmos': {decoded_result}")
     else:
         print("Échec du décodage pour cette trame.")
+
